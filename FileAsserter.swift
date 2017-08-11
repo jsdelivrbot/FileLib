3f81dbf4c815ec49601d02ba48f0a9bd50672055
Eonist <30n1st@gmail.com>
2017-08-11 22:30:25 +0200

    'Files modified: 3'
    '
    
    Modified 3 files:
     FileAsserter.swift
     FileModifier.swift
     FileParser.swift
    '

--- FileAsserter.swift
+++ FileAsserter.swift
@@ -1,9 +1,20 @@
 import Cocoa
+
 class FileAsserter{
     /**
      * EXAMPLE: FileAsserter.exists("~/Desktop/del.txt".tildePath)//true or false (remember to expand the tildePath)
      */
     static func exists(_ path:String) -> Bool {
         return FileManager().fileExists(atPath: path)
+    }
+    /**
+     * New
+     */
+    static func hasContent(_ filePath:String) -> Bool{
+        if let content:[String] = FileParser.contentOfDir(filePath){
+            return !content.isEmpty
+        }else {
+            return false
+        }
     }
 }
